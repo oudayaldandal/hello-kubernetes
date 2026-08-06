f783092e6fadcecd501ba3dd7a2371d3d5f53a18
oudayaldandal <ouday.aldandal.1@ulaval.ca>
2026-08-05 23:25:04 -0400

    Test CI CD pipeline

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
 
-        return "Hello from Kubernetes!";
+        return "Hello from CI/CD Kubernetes!";
     }
 }
